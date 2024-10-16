2319061e67d792e91f7814d4b4f0357288fe0be8
eronryabets <eronryabets@gmail.com>
2024-10-16 21:01:22 +0300

    Book List page - style, effects (CardActionArea).

--- src/components/BookList/BooksList.tsx
+++ src/components/BookList/BooksList.tsx
@@ -7,7 +7,7 @@ import {
     CardContent,
     Typography,
     CircularProgress,
-    Alert, Container, Box, Button, Pagination, Chip,
+    Alert, Container, Box, Button, Pagination, Chip, CardActionArea,
 } from '@mui/material';
 import defaultCover from '../../assets/default_cover.png';
 import {fetchBooks, setCurrentPage} from "../../redux/slices/downloadBookSlice";
@@ -16,7 +16,7 @@ import {Link} from "react-router-dom";
 
 const BooksList: React.FC = () => {
     const dispatch = useAppDispatch();
-    const { books, loading, error, currentPage, totalPages } = useSelector((state: RootState) => state.books);
+    const {books, loading, error, currentPage, totalPages} = useSelector((state: RootState) => state.books);
 
     useEffect(() => {
         dispatch(fetchBooks(currentPage));
@@ -34,21 +34,21 @@ const BooksList: React.FC = () => {
                 alignItems="center"
                 height="80vh"
             >
-                <CircularProgress />
+                <CircularProgress/>
             </Box>
         );
     }
 
     if (error) {
         return (
-            <Container sx={{ mt: 4 }}>
+            <Container sx={{mt: 4}}>
                 <Alert severity="error">{error}</Alert>
             </Container>
         );
     }
 
     return (
-        <Container sx={{ mt: 4, mb: 4 }}>
+        <Container sx={{mt: 4, mb: 4}}>
             <Box
                 display="grid"
                 gap={4}
@@ -59,71 +59,108 @@ const BooksList: React.FC = () => {
                 }}
             >
                 {books.map((book) => (
-                    <Card key={book.id} sx={{ display: 'flex', flexDirection: 'column', height: '100%' }}>
-                        {/* Обложка Книги */}
-                        <Box sx={{ display: 'flex', justifyContent: 'center', mt: 2 }}>
-                            <CardMedia
-                                component="img"
-                                image={book.cover_image || defaultCover}
-                                alt={book.title}
-                                sx={{
-                                    width: 120, // Фиксированная ширина
-                                    height: 160, // Фиксированная высота
-                                    objectFit: 'cover',
-                                    borderRadius: '16px',
-                                    boxShadow: 3, // Добавление тени для эстетики
-                                }}
-                                onError={(e: React.SyntheticEvent<HTMLImageElement, Event>) => {
-                                    e.currentTarget.src = defaultCover;
-                                }}
-                            />
-                        </Box>
-                        {/* Контент Карточки */}
-                        <CardContent sx={{ flexGrow: 1, textAlign: 'center' }}>
-                            <Typography variant="h6" component="div" gutterBottom>
-                                {book.title}
-                            </Typography>
-                            <Typography variant="body2" color="text.secondary">
-                                Добавлено: {new Date(book.created_at).toLocaleDateString()}
-                            </Typography>
-                            {/* Отображение Жанров */}
-                            <Box
+                    <Link
+                        to={`/book/${book.id}`} // Путь к странице детали книги
+                        key={book.id}
+                        style={{textDecoration: 'none', color: 'inherit'}} // Убираем подчёркивание и наследуем цвет
+                    >
+                        <Card
+                            key={book.id}
+                            sx={{
+                                display: 'flex',
+                                flexDirection: 'column',
+                                height: '100%',
+                                transition: 'background-color 0.3s, box-shadow 0.3s', // Плавный переход
+                                '&:hover': {
+                                    // backgroundColor: 'rgba(0, 0, 255, 0.1)', // Светло-синий фон при наведении
+                                    boxShadow: '0px 4px 20px rgba(0, 0, 255, 0.2)', // Усиление тени при наведении
+                                    transform: 'scale(1.01)', //чуть увеличиваем
+                                },
+                                cursor: 'pointer',
+                                '&:focus': {
+                                    outline: '2px solid rgba(0, 0, 255, 0.5)', // Обводка при фокусе
+                                },
+                                // transition: 'background-color 0.3s, box-shadow 0.3s, transform 0.3s',
+                            }}
+                        >
+                            {/* Доп эффекти внутрянки - увеличиваем картинку */}
+                            <CardActionArea
                                 sx={{
                                     display: 'flex',
-                                    flexWrap: 'wrap',
-                                    justifyContent: 'center',
-                                    gap: 1, // Отступы между чипами
-                                    mt: 1,
+                                    flexDirection: 'column',
+                                    height: '100%',
+                                    transition: 'background-color 0.3s, box-shadow 0.3s, transform 0.3s',
+                                    '&:hover img': {
+                                        boxShadow: '0 0 20px rgba(0, 0, 255, 0.5)', // Свечение вокруг изображения
+                                        // transform: 'rotateY(20deg) scale(1.05)', // Поворот вокруг оси Y и масштабирование
+                                        filter: 'brightness(1.2) contrast(1.1)', // Увеличение яркости и контраста
+                                        animation: 'pulse 2.5s infinite',
+                                    },
+                                    '@keyframes pulse': {
+                                        '0%': { transform: 'scale(1)' },
+                                        '50%': { transform: 'scale(1.05)' },
+                                        '100%': { transform: 'scale(1)' },
+                                    },
                                 }}
                             >
-                                {book.genre_details.map((genre) => (
-                                    <Chip
-                                        key={genre.id}
-                                        label={genre.name}
-                                        variant="outlined"
-                                        color="primary"
-                                        size="small"
+
+                                {/* Обложка Книги */}
+                                <Box sx={{display: 'flex', justifyContent: 'center', mt: 2}}>
+                                    <CardMedia
+                                        component="img"
+                                        image={book.cover_image || defaultCover}
+                                        alt={book.title}
+                                        sx={{
+                                            width: 120, // Фиксированная ширина
+                                            height: 160, // Фиксированная высота
+                                            objectFit: 'cover',
+                                            borderRadius: '16px',
+                                            boxShadow: 3, // Добавление тени для эстетики
+                                            transition: 'transform 0.3s', // Плавный переход для трансформаций
+                                        }}
+                                        onError={(e: React.SyntheticEvent<HTMLImageElement, Event>) => {
+                                            e.currentTarget.src = defaultCover;
+                                        }}
                                     />
-                                ))}
-                            </Box>
-                            {/* Дополнительная Информация */}
-                            <Box sx={{ mt: 2 }}>
-                                <Typography variant="body2" color="text.secondary">
-                                    Количество глав: {book.chapters.length}
-                                </Typography>
-                                {/* Ссылка на подробную информацию о книге */}
-                                <Button
-                                    variant="outlined"
-                                    size="small"
-                                    component={Link}
-                                    to={`/book/${book.id}`}
-                                    sx={{ mt: 1 }}
-                                >
-                                    Подробнее
-                                </Button>
-                            </Box>
-                        </CardContent>
-                    </Card>
+                                </Box>
+                                {/* Контент Карточки */}
+                                <CardContent sx={{flexGrow: 1, textAlign: 'center'}}>
+                                    <Typography variant="h6" component="div" gutterBottom>
+                                        {book.title}
+                                    </Typography>
+                                    <Typography variant="body2" color="text.secondary">
+                                        Добавлено: {new Date(book.created_at).toLocaleDateString()}
+                                    </Typography>
+                                    {/* Отображение Жанров */}
+                                    <Box
+                                        sx={{
+                                            display: 'flex',
+                                            flexWrap: 'wrap',
+                                            justifyContent: 'center',
+                                            gap: 1, // Отступы между чипами
+                                            mt: 1,
+                                        }}
+                                    >
+                                        {book.genre_details.map((genre) => (
+                                            <Chip
+                                                key={genre.id}
+                                                label={genre.name}
+                                                variant="outlined"
+                                                color="primary"
+                                                size="small"
+                                            />
+                                        ))}
+                                    </Box>
+                                    {/* Дополнительная Информация */}
+                                    <Box sx={{mt: 2}}>
+                                        <Typography variant="body2" color="text.secondary">
+                                            Количество глав: {book.chapters.length}
+                                        </Typography>
+                                    </Box>
+                                </CardContent>
+                            </CardActionArea>
+                        </Card>
+                    </Link>
                 ))}
             </Box>
             {/* Пагинация */}
